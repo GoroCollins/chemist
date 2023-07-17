ed5a0bbf6313e864e7c741302ebd6ed7f8fb86be
Goro Collins <gorocollins@gmail.com>
2023-07-17 21:29:44 +0300

    templates updates

--- inventory/views.py
+++ inventory/views.py
@@ -14,6 +14,8 @@ def index(request):
     num_vendors = Vendor.objects.count()
     num_invoices = SalesHeader.objects.count()
     num_lpos = PurchaseHeader.objects.count()
+    pending_lpo = PurchaseHeader.objects.filter(status=1).count()
+    open_lpo = PurchaseHeader.objects.filter(status=0).count()
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
 
@@ -23,36 +25,46 @@ def index(request):
         'num_invoices': num_invoices,
         'num_lpos': num_lpos,
         'num_visits': num_visits,
+        'pending_lpo': pending_lpo,
+        'open_lpo': open_lpo,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'inventory/index.html', context=context)
 class VendorListView(generic.ListView):
     model = Vendor
+    paginate_by = 25
 
 class VendorDetailView(generic.DetailView):
     model = Vendor
+    paginate_by = 25
 
 class ItemListView(generic.ListView):
     model = Item
+    paginate_by = 25
 
 class ItemDetailView(generic.DetailView):
     model = Item
+    paginate_by = 25
 
 class PurchaseOrderListView(generic.DetailView):
     model = PurchaseHeader
+    paginate_by = 25
 
 class PurchaseOrderDetailView(generic.DetailView):
     model = PurchaseHeader
 
 class SalesInvoiceListView(generic.ListView):
     model = SalesHeader
+    paginate_by = 25
 
 class SalesInvoiceDetailView(generic.DetailView):
     model = SalesHeader
+    paginate_by = 25
 
 class ApprovalListView(generic.ListView):
     model = ApprovalEntry
+    paginate_by = 10
 
 
 
