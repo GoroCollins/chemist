31b862aeb5c2de4b8bd2cb6c28b790f738c7d2d6
Goro Collins <gorocollins@gmail.com>
2023-07-27 07:33:25 +0300

    multiple forms together

--- inventory/forms.py
+++ inventory/forms.py
@@ -3,14 +3,6 @@ from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 from . models import Item, ItemEntry, Unit, Vendor, PurchaseHeader, PurchaseLine, PurchaseCreditMemoHeader, PurchaseCreditMemoLine, SalesHeader, SalesLines, SalesCreditMemoHeader, SalesCreditMemoLine
 
-class UnitForm(forms.ModelForm):
-    class Meta:
-        model = Unit
-        fields = ['code', 'description']
-        labels = {'code': _('Code'), 
-                  'description':_('Description')}
-        error_messages = {'code': {'max_length': _('The code value is too long')},
-                          'description':{'max_length':_('The description is too long')}}
 
 class SalesHeaderForm(forms.ModelForm):
     model = SalesHeader
