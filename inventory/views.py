ddbc632fbd15eb00936ff7160fca8cb9b122928d
Goro Collins <gorocollins@gmail.com>
2023-12-04 21:32:03 +0300

    Miscellanous updates

--- inventory/views.py
+++ inventory/views.py
@@ -433,9 +433,6 @@ class SalesCreditMemoHeaderInline():
         if not all((x.is_valid() for x in named_formsets.values())):
             return self.render_to_response(self.get_context_data(form=form))
         self.object = form.save()
-
-        # for every formset, attempt to find a specific formset save function
-        # otherwise, just save.
         for name, formset in named_formsets.items():
             formset_save_func = getattr(self, 'formset_{0}_valid'.format(name), None)
             if formset_save_func is not None:
@@ -486,9 +483,6 @@ class PurchaseCreditMemoHeaderInline():
         if not all((x.is_valid() for x in named_formsets.values())):
             return self.render_to_response(self.get_context_data(form=form))
         self.object = form.save()
-
-        # for every formset, attempt to find a specific formset save function
-        # otherwise, just save.
         for name, formset in named_formsets.items():
             formset_save_func = getattr(self, 'formset_{0}_valid'.format(name), None)
             if formset_save_func is not None:
@@ -687,7 +681,7 @@ def purchases_pdf(request, pk):
     elements = []
 
     # Load and add an image to the top-right frame
-    image_path = '/home/goro/projects/inventory/chemist/inventory/static/inventory/images/mypic.png'  # Replace with the actual path to your image
+    image_path = '/home/goro/projects/inventory/inventweb/chemist/inventory/static/inventory/images/mypic.png'  # Replace with the actual path to your image
     img = Image(image_path, width=200, height=100)  # Adjust width and height as needed
     img.hAlign = 'RIGHT'  # Align the image to the right within the frame
 
