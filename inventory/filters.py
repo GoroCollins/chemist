f1f3b2383e62c720a5eeb53cc2a0b16f289627ba
Goro Collins <gorocollins@gmail.com>
2023-12-08 07:17:51 +0300

    Add users filter to the items list

--- inventory/filters.py
+++ inventory/filters.py
@@ -2,8 +2,18 @@ import django_filters
 
 from . models import Item
 
+from django.forms import forms
+
+from django.contrib.auth.models import User
+
 class ItemFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
+    created_by = django_filters.ModelChoiceFilter(
+        queryset=User.objects.all(),
+        empty_label="All Users",
+        label="Users",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        )
     
     class Meta:
         model = Item 
