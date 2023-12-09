d6e1bd7d8ac9ed720eff6cff2ade4fe8a960aa6c
Goro Collins <gorocollins@gmail.com>
2023-12-09 12:16:12 +0300

    Updated the item code and descriptions to contains filter and units to exact filter

--- inventory/filters.py
+++ inventory/filters.py
@@ -7,13 +7,15 @@ from django.forms import forms
 from django.contrib.auth.models import User
 
 class ItemFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    code = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'class': 'form-control'}))
     created_by = django_filters.ModelChoiceFilter(
         queryset=User.objects.all(),
         empty_label="All Users",
         label="Users",
         widget=forms.Select(attrs={'class': 'form-control'}),
         )
+    description = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    unit = django_filters.CharFilter(lookup_expr='exact', widget=forms.TextInput(attrs={'class': 'form-control'}))
     
     class Meta:
         model = Item 
